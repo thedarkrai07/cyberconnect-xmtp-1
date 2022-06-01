c01126a511cd782c3cf1ec87b19275ba9b740206
thedarkrai07 <tothedarkrai07@gmail.com>
2022-06-01 11:46:46 +0530

    added github link to the project

--- src/WebsitePlaceholder.tsx
+++ src/WebsitePlaceholder.tsx
@@ -1,10 +1,18 @@
-import { Stack, Text } from "@chakra-ui/react";
+import { Link, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 import { LoremIpsum } from "react-lorem-ipsum";
 
 const WebsitePlaceholder = () => {
     return (
         <Stack>
+            <Link
+                p={10}
+                fontSize="2xl"
+                href="https://github.com/thedarkrai07/cyberconnect-xmtp-1"
+                target="_blank"
+            >
+                Github Link to the Project
+            </Link>
             <Text p={10} fontSize="3xl" align={"center"}>
                 Sample Website
             </Text>
