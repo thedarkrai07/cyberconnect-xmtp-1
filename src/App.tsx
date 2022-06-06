779240dc3361c534e76035377e6653adc9d36363
thedarkrai07 <tothedarkrai07@gmail.com>
2022-06-06 10:40:16 +0530

    theme mode exposed

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,30 @@ import { GraphContextProvider } from "./context/GraphContext";
 import { Web3ContextProvider } from "./context/web3Context";
 import client from "./graphql/client";
 
-const theme = extendTheme({
-    config: {
-        initialColorMode: "light",
-        useSystemColorMode: false,
-    },
-    components: {
-        Modal: {
-            baseStyle: () => ({
-                dialog: {
-                    bg: "#000",
-                    boxShadow:
-                        "rgb(255 255 255 / 20%) 0px 0px 15px, rgb(255 255 255 / 15%) 0px 0px 3px 1px",
-                },
-            }),
+function App({ themeMode }: { themeMode: string }) {
+    const theme = extendTheme({
+        config: {
+            initialColorMode: themeMode || "dark",
+            useSystemColorMode: false,
         },
-        Divider: {
-            baseStyle: () => ({
-                borderColor: "#FFF",
-            }),
+        components: {
+            Modal: {
+                baseStyle: () => ({
+                    dialog: {
+                        bg: "#000",
+                        boxShadow:
+                            "rgb(255 255 255 / 20%) 0px 0px 15px, rgb(255 255 255 / 15%) 0px 0px 3px 1px",
+                    },
+                }),
+            },
+            Divider: {
+                baseStyle: () => ({
+                    borderColor: "#FFF",
+                }),
+            },
         },
-    },
-});
+    });
 
-function App() {
     return (
         <ApolloProvider client={client}>
             <XmtpProvider>
